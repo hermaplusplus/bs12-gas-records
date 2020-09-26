24d4b9c3db48cea95f89025adb7f58b52677df19
herma <49307226+hermaplusplus@users.noreply.github.com>
2020-09-26 14:22:09 +0100

    Adds icons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,8 +35,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace bs12_gas_records
 {
@@ -15,6 +16,11 @@ namespace bs12_gas_records
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = new Icon("xynergy.ico");
         }
     }
 }

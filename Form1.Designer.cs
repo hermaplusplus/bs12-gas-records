1757f6cd8cad6591cf02e2a302ce82124b44f3e4
herma <49307226+hermaplusplus@users.noreply.github.com>
2020-09-26 14:33:56 +0100

    Secret icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Giant Armoured Serpentid Record Utility";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
